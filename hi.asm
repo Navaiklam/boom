b7255a96bdab8384c8f9663b3d7f083458e84c60
Hafdly <dmlk.dark@gmail.com>
2018-03-24 05:39:21 +0000

    Hello World bin elf .386

--- hi.asm
+++ hi.asm
@@ -1,5 +1,6 @@
 section .data
-msg db "Hola Mundoooooooooo!!",0xA,0xD
+msg db "Hola Mundo!!",0xA,0xD
+len equ $ - msg
 ;; db Define Byte Allocates 1 byte 'a'
 ;; dw Define Word Allotates 2 byte -+ num 
 ;; dd Dfine Doubleword Allotates 4 byte 
@@ -7,3 +8,13 @@ msg db "Hola Mundoooooooooo!!",0xA,0xD
 ;; dt Define Ten Byte Allotates 10 byte 
 
 section .text
+	global _start
+_start:
+	mov eax, 4 ;; destino, origien, syscall(4) sys_write ;; destino = origen 
+	mov ebx, 1 ;; = destino origen ;stdont
+	mov ecx, msg ;; msg pantalla
+	mov edx, len ;; longitud del mensaje
+	int 0x80	;; llamada al sistema de interrumpuciones ; syscall
+	mov eax, 1 ;(system, exit) sys_exit)
+	int 0x80
+
