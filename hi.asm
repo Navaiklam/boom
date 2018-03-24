7906dd810e8f342dd6c29924bfe5a134b374ee74
Hafdly <dmlk.dark@gmail.com>
2018-03-24 04:01:47 +0000

    We extend comments

--- hi.asm
+++ hi.asm
@@ -1,9 +1,9 @@
 section .data
 msg db "Hola Mundoooooooooo!!",0xA,0xD
-;; db byte
-;; dw palabra
-;; dd palabra doble
-;; dq palabra cuadruple
-;; dt diez byte
+;; db Define Byte Allocates 1 byte 'a'
+;; dw Define Word Allotates 2 byte -+ num 
+;; dd Dfine Doubleword Allotates 4 byte 
+;; dq Define Quadword Allotates 8 byte big num
+;; dt Define Ten Byte Allotates 10 byte 
 
 section .text
